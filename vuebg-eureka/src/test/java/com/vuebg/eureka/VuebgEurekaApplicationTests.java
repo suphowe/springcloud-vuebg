28effe4c2d72b5671d0885617dfd8bd8a36a9140
suphowe <suphowe@gmail.com>
2019-12-10 15:24:36 +0800

    eureka注册中心

---
+++ vuebg-eureka/src/test/java/com/vuebg/eureka/VuebgEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuebg.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuebgEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
