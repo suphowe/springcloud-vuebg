a5f4db96b52c220333036eb071499a11b3254c10
suphowe <suphowe@gmail.com>
2019-12-11 14:28:58 +0800

    vuebg-backup数据初始化

---
+++ vuebg-backup/src/main/java/com/vuebg/backup/BackupApplication.java
@@ -0,0 +1,32 @@
+package com.vuebg.backup;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@MapperScan("com.vuebg.backup.dao")
+public class BackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackupApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
---
+++ vuebg-backup/src/test/java/com/vuebg/backup/BackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuebg.backup;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
